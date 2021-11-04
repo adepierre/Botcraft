7ea0e1334ef8122867c32a3fb3bbc2b06a574dd9
JackOBIsReal <31803551+JackOBIsReal@users.noreply.github.com>
2021-11-04 23:35:03 +0100

    Apply suggestions from code review
    Co-authored-by: adepierre <24371370+adepierre@users.noreply.github.com>

--- botcraft/src/Game/Inventory/InventoryManager.cpp
+++ botcraft/src/Game/Inventory/InventoryManager.cpp
@@ -288,13 +288,12 @@ namespace Botcraft
         InventoryType type = InventoryType::Default;
         if (msg.GetType() == "minecraft:chest")
         {
-            //std::cout << (int)msg.GetNumberOfSlots() << std::endl;
             switch (msg.GetNumberOfSlots())
             {
-            case 9*3://62:
+            case 9*3:
                 type = InventoryType::Generic9x3;
                 break;
-            case 9*6://89:
+            case 9*6:
                 type = InventoryType::Generic9x6;
                 break;
             default:
