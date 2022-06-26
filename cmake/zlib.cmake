9c341b77f7911ed889ada7c2c2ea0e572019ef87
adepierre <24371370+adepierre@users.noreply.github.com>
2022-06-26 16:03:00 +0200

    Fix zlib using dynamic library instead of static one

--- cmake/zlib.cmake
+++ cmake/zlib.cmake
@@ -9,7 +9,6 @@ if(NOT TARGET ZLIB::ZLIB)
     
     file(GLOB RESULT "${CMAKE_CURRENT_SOURCE_DIR}/3rdparty/zlib/examples")
     list(LENGTH RESULT RES_LEN)
-    message(STATUS "${RES_LEN}")
     if(RES_LEN EQUAL 0)
         message(STATUS "Zlib not found, cloning it...")
         execute_process(COMMAND git submodule update --init -- 3rdparty/zlib WORKING_DIRECTORY "${CMAKE_CURRENT_SOURCE_DIR}")
@@ -27,6 +26,19 @@ if(NOT TARGET ZLIB::ZLIB)
 	execute_process(COMMAND "cmake" "--build" "." "--target" "install" "--config" "Release" WORKING_DIRECTORY "${ZLIB_BUILD_PATH}")
 
 	# Find the freshly built library
+    
+    # From 3.24 there is a cmake option to find zlib static
+    # but before, we need to do it in a more manual way
+    if(${CMAKE_VERSION} VERSION_LESS "3.24.0")
+        set(_CMAKE_FIND_LIBRARY_SUFFIXES ${CMAKE_FIND_LIBRARY_SUFFIXES})
+        set(CMAKE_FIND_LIBRARY_SUFFIXES "static.lib" ".a")
+    else()
+        set(ZLIB_USE_STATIC_LIBS "ON")
+    endif()
 	set(ZLIB_ROOT "${ZLIB_BUILD_PATH}/install")
 	find_package(ZLIB QUIET)
+    if(${CMAKE_VERSION} VERSION_LESS "3.24.0")
+        set(CMAKE_FIND_LIBRARY_SUFFIXES ${_CMAKE_FIND_LIBRARY_SUFFIXES}) 
+        unset(_CMAKE_FIND_LIBRARY_SUFFIXES)
+    endif()
 endif()
