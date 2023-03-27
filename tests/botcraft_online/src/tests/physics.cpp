06769481ea915f0655077abf4c57d2b5c33aaeda
adepierre <24371370+adepierre@users.noreply.github.com>
2023-03-27 21:59:45 +0200

    Add physics tests

---
+++ tests/botcraft_online/src/tests/physics.cpp
@@ -0,0 +1,60 @@
+#include "TestManager.hpp"
+#include "MinecraftServer.hpp"
+#include "Utils.hpp"
+
+#include <catch2/catch_test_macros.hpp>
+#include <catch2/generators/catch_generators.hpp>
+
+#include <botcraft/Network/NetworkManager.hpp>
+#include <botcraft/Game/Entities/EntityManager.hpp>
+#include <botcraft/Game/Entities/LocalPlayer.hpp>
+
+#include <sstream>
+
+TEST_CASE("gravity")
+{
+	std::unique_ptr<Botcraft::ManagersClient> bot = SetupTestBot(Botcraft::Vector3<double>(1.0, 2.0, 1.0));
+	std::shared_ptr<Botcraft::LocalPlayer> local_player = bot->GetEntityManager()->GetLocalPlayer();
+
+	CHECK(Botcraft::WaitForCondition([&]()
+		{
+			std::lock_guard<std::mutex> lock(local_player->GetMutex());
+			return local_player->GetPosition().y < 2.2 && local_player->GetOnGround();
+		}, 2000));
+	const Botcraft::Vector3<double> target_pos = Botcraft::Vector3<double>(1.5, 0.0, 1.5) + TestManager::GetInstance().GetCurrentOffset();
+	std::lock_guard<std::mutex> lock(local_player->GetMutex());
+	CHECK(local_player->GetPosition().SqrDist(target_pos) < 0.2);
+}
+
+TEST_CASE("collisions")
+{
+	std::unique_ptr<Botcraft::ManagersClient> bot = SetupTestBot(Botcraft::Vector3<double>(2.5, 1.01, 2.5));
+
+	std::vector<std::pair<std::string, Botcraft::Vector3<double>>> directions = {
+		{"south", Botcraft::Vector3<double>(0.0, 0.0, 1.0)},
+		{"north", Botcraft::Vector3<double>(0.0, 0.0, -1.0)},
+		{"east", Botcraft::Vector3<double>(1.0, 0.0, 0.0)},
+		{"west", Botcraft::Vector3<double>(-1.0, 0.0, 0.0)},
+	};
+
+	std::shared_ptr<Botcraft::LocalPlayer> local_player = bot->GetEntityManager()->GetLocalPlayer();
+	const Botcraft::Vector3<double> init_position = local_player->GetPosition();
+	for (size_t i = 0; i < directions.size(); ++i)
+	{
+		SECTION(directions[i].first)
+		{
+			// Walk in the given direction for a total of 2m
+			for (size_t j = 0; j < 20; ++j)
+			{
+				{
+					std::lock_guard<std::mutex> lock(local_player->GetMutex());
+					local_player->AddPlayerInputs(directions[i].second * 0.1);
+				}
+				Botcraft::SleepFor(std::chrono::milliseconds(30));
+			}
+			// Check the wall stopped the walk
+			std::lock_guard<std::mutex> lock(local_player->GetMutex());
+			CHECK(local_player->GetPosition().SqrDist(init_position + directions[i].second * 1.2) < 0.2);
+		}
+	}
+}
