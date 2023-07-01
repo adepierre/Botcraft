9492e665d74d4c187bfe2a484ff95238f9063196
adepierre <24371370+adepierre@users.noreply.github.com>
2023-07-01 20:11:24 +0200

    Fix doc for demangling utilities

--- botcraft/include/botcraft/Utilities/DemanglingUtilities.hpp
+++ botcraft/include/botcraft/Utilities/DemanglingUtilities.hpp
@@ -8,7 +8,7 @@ namespace Botcraft
     {
         /// @brief Demangle a class name
         /// @param name Raw output of typeid().name()
-        /// @param remove_template If true, will simplify the name to get only the class name without any template <parameters> or namespace prefix
+        /// @param simplify_output If true, will simplify the name to get only the class name without any template parameters or namespace prefix
         /// @return Demangled name
         std::string Demangle(const std::string& name, const bool simplify_output);
     }
