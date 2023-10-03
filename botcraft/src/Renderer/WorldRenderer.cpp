dc936370cbe61a7b96301eb569b9bf5847fdecd3
adepierre <24371370+adepierre@users.noreply.github.com>
2023-10-03 19:50:38 +0200

    Better rendering of redstone wires color

--- botcraft/src/Renderer/WorldRenderer.cpp
+++ botcraft/src/Renderer/WorldRenderer.cpp
@@ -576,34 +576,12 @@ namespace Botcraft
                         }
                     }
                     break;
-                    //Something like black when signal strength is 0 and red when it's 15
+                    // Black when signal strength is 0 and red when it's 15
                 case TintType::Redstone:
 #if PROTOCOL_VERSION == 340 /* 1.12.2 */
-                    texture_modifier[i] = 0xFF000000 | (25 + 15 * blockstate->GetMetadata());
-#elif PROTOCOL_VERSION == 393 /* 1.13 */
-                    texture_modifier[i] = 0xFF000000 | (25 + 15 * (((blockstate->GetId() - 1752) / 9) % 16));
-#elif PROTOCOL_VERSION == 401 /* 1.13.1 */ || PROTOCOL_VERSION == 404 /* 1.13.2 */
-                    texture_modifier[i] = 0xFF000000 | (25 + 15 * (((blockstate->GetId() - 1753) / 9) % 16));
-#elif PROTOCOL_VERSION == 477 /* 1.14 */ || PROTOCOL_VERSION == 480 /* 1.14.1 */ || PROTOCOL_VERSION == 485 /* 1.14.2 */ || PROTOCOL_VERSION == 490 /* 1.14.3 */ || PROTOCOL_VERSION == 498 /* 1.14.4 */
-                    texture_modifier[i] = 0xFF000000 | (25 + 15 * (((blockstate->GetId() - 2056) / 9) % 16));
-#elif PROTOCOL_VERSION == 573 /* 1.15 */ || PROTOCOL_VERSION == 575 /* 1.15.1 */ || PROTOCOL_VERSION == 578 /* 1.15.2 */
-                    texture_modifier[i] = 0xFF000000 | (25 + 15 * (((blockstate->GetId() - 2056) / 9) % 16));
-#elif PROTOCOL_VERSION == 735 /* 1.16 */ || PROTOCOL_VERSION == 736 /* 1.16.1 */ || PROTOCOL_VERSION == 751 /* 1.16.2 */ || PROTOCOL_VERSION == 753 /* 1.16.3 */  || PROTOCOL_VERSION == 754 /* 1.16.4/5 */
-                    texture_modifier[i] = 0xFF000000 | (25 + 15 * (((blockstate->GetId() - 2058) / 9) % 16));
-#elif PROTOCOL_VERSION == 755 /* 1.17 */ || PROTOCOL_VERSION == 756 /* 1.17.1 */
-                    texture_modifier[i] = 0xFF000000 | (25 + 15 * (((blockstate->GetId() - 2114) / 9) % 16));
-#elif PROTOCOL_VERSION == 757 /* 1.18/.1 */ || PROTOCOL_VERSION == 758 /* 1.18.2 */
-                    texture_modifier[i] = 0xFF000000 | (25 + 15 * (((blockstate->GetId() - 2114) / 9) % 16));
-#elif PROTOCOL_VERSION == 759 /* 1.19 */ || PROTOCOL_VERSION == 760 /* 1.19.1/2 */
-                    texture_modifier[i] = 0xFF000000 | (25 + 15 * (((blockstate->GetId() - 2312) / 9) % 16));
-#elif PROTOCOL_VERSION == 759 /* 1.19 */ || PROTOCOL_VERSION == 761 /* 1.19.3 */
-                    texture_modifier[i] = 0xFF000000 | (25 + 15 * (((blockstate->GetId() - 2926) / 9) % 16));
-#elif PROTOCOL_VERSION == 759 /* 1.19 */ || PROTOCOL_VERSION == 762 /* 1.19.4 */
-                    texture_modifier[i] = 0xFF000000 | (25 + 15 * (((blockstate->GetId() - 2974) / 9) % 16));
-#elif PROTOCOL_VERSION == 763 /* 1.20/.1 */ || PROTOCOL_VERSION == 764 /* 1.20.2 */
-                    texture_modifier[i] = 0xFF000000 | (25 + 15 * (((blockstate->GetId() - 2978) / 9) % 16));
+                    texture_modifier[i] = 0xFF000000 | (25 + 15 * blockstate->GetId().second);
 #else
-        #error "Protocol version not implemented"
+                    texture_modifier[i] = 0xFF000000 | (25 + 15 * std::stoi(blockstate->GetVariableValue("power")));
 #endif
                     break;
                 case TintType::Water:
