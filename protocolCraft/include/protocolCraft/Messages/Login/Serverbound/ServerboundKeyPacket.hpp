160dd4aef25a1d0679b75fdb325121310ed50fd8
adepierre <24371370+adepierre@users.noreply.github.com>
2024-06-17 22:42:52 +0200

    Hotfix cause online mode was not working and it's kinda annoying

--- protocolCraft/include/protocolCraft/Messages/Login/Serverbound/ServerboundKeyPacket.hpp
+++ protocolCraft/include/protocolCraft/Messages/Login/Serverbound/ServerboundKeyPacket.hpp
@@ -45,47 +45,47 @@ namespace ProtocolCraft
     protected:
         virtual void ReadImpl(ReadIterator& iter, size_t& length) override
         {
-            key_bytes = ReadData<std::vector<unsigned char>>(iter, length);
+            SetKeyBytes(ReadData<std::vector<unsigned char>>(iter, length));
 #if PROTOCOL_VERSION < 761 /* < 1.19.3 */
 #if PROTOCOL_VERSION > 758 /* > 1.18.2 */
             const bool has_nonce = ReadData<bool>(iter, length);
             if (has_nonce)
             {
                 const int nonce_length = ReadData<VarInt>(iter, length);
-                nonce = ReadByteArray(iter, length, nonce_length);
+                SetNonce(ReadByteArray(iter, length, nonce_length));
             }
             else
             {
-                salt_signature = ReadData<SaltSignature>(iter, length);
+                SetSaltSignature(ReadData<SaltSignature>(iter, length));
             }
 #else
-            nonce = ReadData<std::vector<unsigned char>>(iter, length);
+            SetNonce(ReadData<std::vector<unsigned char>>(iter, length));
 #endif
 #else
-            encrypted_challenge = ReadData<std::vector<unsigned char>>(iter, length);
+            SetEncryptedChallenge(ReadData<std::vector<unsigned char>>(iter, length));
 #endif
         }
 
         virtual void WriteImpl(WriteContainer& container) const override
         {
-            WriteData<std::vector<unsigned char>>(key_bytes, container);
+            WriteData<std::vector<unsigned char>>(GetKeyBytes(), container);
 #if PROTOCOL_VERSION < 761 /* < 1.19.3 */
 #if PROTOCOL_VERSION > 758 /* > 1.18.2 */
-            WriteData<bool>(salt_signature.GetSignature().empty(), container);
-            if (salt_signature.GetSignature().empty())
+            WriteData<bool>(GetSaltSignature().GetSignature().empty(), container);
+            if (GetSaltSignature().GetSignature().empty())
             {
-                WriteData<VarInt>(static_cast<int>(nonce.size()), container);
-                WriteByteArray(nonce, container);
+                WriteData<VarInt>(static_cast<int>(GetNonce().size()), container);
+                WriteByteArray(GetNonce(), container);
             }
             else
             {
-                WriteData<SaltSignature>(salt_signature, container);
+                WriteData<SaltSignature>(GetSaltSignature(), container);
             }
 #else
-            WriteData<std::vector<unsigned char>>(nonce, container);
+            WriteData<std::vector<unsigned char>>(GetNonce(), container);
 #endif
 #else
-            WriteData<std::vector<unsigned char>>(encrypted_challenge, container);
+            WriteData<std::vector<unsigned char>>(GetEncryptedChallenge(), container);
 #endif
         }
 
@@ -93,36 +93,25 @@ namespace ProtocolCraft
         {
             Json::Value output;
 
-            output["key_bytes"] = "vector of " + std::to_string(key_bytes.size()) + " unsigned char";
+            output[std::string(json_names[static_cast<size_t>(FieldsEnum::KeyBytes)])] = "Vector of " + std::to_string(GetKeyBytes().size()) + " unsigned char";
 #if PROTOCOL_VERSION < 761 /* < 1.19.3 */
 #if PROTOCOL_VERSION > 758 /* > 1.18.2 */
-            if (salt_signature.GetSignature().empty())
+            if (GetSaltSignature().GetSignature().empty())
             {
-                output["nonce"] = "vector of " + std::to_string(nonce.size()) + " unsigned char";
+                output[std::string(json_names[static_cast<size_t>(FieldsEnum::Nonce)])] = "Vector of " + std::to_string(GetNonce().size()) + " unsigned char";
             }
             else
             {
-                output["salt_signature"] = salt_signature;
+                output[std::string(json_names[static_cast<size_t>(FieldsEnum::SaltSignature)])] = GetSaltSignature();
             }
 #else
-            output["nonce"] = "vector of " + std::to_string(nonce.size()) + " unsigned char";
+            output[std::string(json_names[static_cast<size_t>(FieldsEnum::Nonce)])] = "Vector of " + std::to_string(GetNonce().size()) + " unsigned char";
 #endif
 #else
-            output["encrypted_challenge"] = "vector of " + std::to_string(encrypted_challenge.size()) + " unsigned char";
+            output[std::string(json_names[static_cast<size_t>(FieldsEnum::EncryptedChallenge)])] = "Vector of " + std::to_string(GetEncryptedChallenge().size()) + " unsigned char";
 #endif
 
             return output;
         }
-
-    private:
-        std::vector<unsigned char> key_bytes;
-#if PROTOCOL_VERSION < 761 /* < 1.19.3 */
-        std::vector<unsigned char> nonce;
-#if PROTOCOL_VERSION > 758 /* > 1.18.2 */
-        SaltSignature salt_signature;
-#endif
-#else
-        std::vector<unsigned char> encrypted_challenge;
-#endif
     };
 } // ProtocolCraft
