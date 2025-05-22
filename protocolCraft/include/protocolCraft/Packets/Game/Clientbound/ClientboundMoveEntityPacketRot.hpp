621584997c1f7fd92deabccc87f019b82621ed75
adepierre <24371370+adepierre@users.noreply.github.com>
2025-05-22 21:08:25 +0200

    Fix Yaw/Pitch swap in ClientboundMoveEntityPacketRot

--- protocolCraft/include/protocolCraft/Packets/Game/Clientbound/ClientboundMoveEntityPacketRot.hpp
+++ protocolCraft/include/protocolCraft/Packets/Game/Clientbound/ClientboundMoveEntityPacketRot.hpp
@@ -10,8 +10,8 @@ namespace ProtocolCraft
         static constexpr std::string_view packet_name = "Move Entity Rot";
 
         SERIALIZED_FIELD(EntityId, VarInt);
-        SERIALIZED_FIELD(XRot, unsigned char);
         SERIALIZED_FIELD(YRot, unsigned char);
+        SERIALIZED_FIELD(XRot, unsigned char);
         SERIALIZED_FIELD(OnGround, bool);
 
         DECLARE_READ_WRITE_SERIALIZE;
