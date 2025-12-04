93ce6fdf8015392f323c06a988f477aa296e9770
adepierre <24371370+adepierre@users.noreply.github.com>
2025-12-04 22:17:53 +0000

    Hotfix crash when receiving an armor trim recipe

--- protocolCraft/include/protocolCraft/Types/Recipes/SmithingTrimDemoSlotDisplay.hpp
+++ protocolCraft/include/protocolCraft/Types/Recipes/SmithingTrimDemoSlotDisplay.hpp
@@ -3,6 +3,10 @@
 
 #include "protocolCraft/Types/Recipes/SlotDisplayData.hpp"
 #include "protocolCraft/Types/Recipes/SlotDisplay.hpp"
+#if PROTOCOL_VERSION > 769 /* > 1.21.4 */
+#include "protocolCraft/Types/Holder.hpp"
+#include "protocolCraft/Types/Components/Subtypes/TrimPattern.hpp"
+#endif
 
 namespace ProtocolCraft
 {
@@ -10,7 +14,11 @@ namespace ProtocolCraft
     {
         SERIALIZED_FIELD(Base, SlotDisplay);
         SERIALIZED_FIELD(Material, SlotDisplay);
+#if PROTOCOL_VERSION < 770 /* < 1.21.5 */
         SERIALIZED_FIELD(Pattern, SlotDisplay);
+#else
+        SERIALIZED_FIELD(Pattern, Holder<Components::TrimPattern>);
+#endif
 
         DECLARE_READ_WRITE_SERIALIZE;
     };
