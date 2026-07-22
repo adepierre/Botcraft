ded58a5dd577f62d2c3dbfec2854904a094b791f
adepierre <24371370+adepierre@users.noreply.github.com>
2026-07-23 00:53:36 +0100

    Fix ProvidesTrimMaterial component for 26.2+

--- protocolCraft/include/protocolCraft/Types/Components/DataComponentTypeProvidesTrimMaterial.hpp
+++ protocolCraft/include/protocolCraft/Types/Components/DataComponentTypeProvidesTrimMaterial.hpp
@@ -2,9 +2,9 @@
 #pragma once
 #include "protocolCraft/Types/Components/DataComponentType.hpp"
 #include "protocolCraft/Types/Components/Subtypes/TrimMaterial.hpp"
+#include "protocolCraft/Types/Holder.hpp"
 #if PROTOCOL_VERSION < 775 /* < 26.1 */
 #include "protocolCraft/Types/Either.hpp"
-#include "protocolCraft/Types/Holder.hpp"
 #include "protocolCraft/Types/Identifier.hpp"
 #endif
 
@@ -17,7 +17,7 @@ namespace ProtocolCraft
 #if PROTOCOL_VERSION < 775 /* < 26.1 */
             SERIALIZED_FIELD(Material, Either<Holder<ProtocolCraft::Components::TrimMaterial>, Identifier>);
 #else
-            SERIALIZED_FIELD(Material, ProtocolCraft::Components::TrimMaterial);
+            SERIALIZED_FIELD(Material, Holder<ProtocolCraft::Components::TrimMaterial>);
 #endif
 
             DECLARE_READ_WRITE_SERIALIZE;
