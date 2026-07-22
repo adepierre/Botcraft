40693640d486bcaaa742256f4e0d0dfb288df1a4
adepierre <24371370+adepierre@users.noreply.github.com>
2026-07-23 00:51:50 +0100

    Fix instrument component for 1.21.1- and 26.2+

--- protocolCraft/include/protocolCraft/Types/Components/DataComponentTypeInstrument.hpp
+++ protocolCraft/include/protocolCraft/Types/Components/DataComponentTypeInstrument.hpp
@@ -2,7 +2,7 @@
 #pragma once
 #include "protocolCraft/Types/Components/DataComponentType.hpp"
 #include "protocolCraft/Types/Holder.hpp"
-#if PROTOCOL_VERSION > 769 /* > 1.21.4 */ && PROTOCOL_VERSION < 775 /* > 26.1 */
+#if PROTOCOL_VERSION > 769 /* > 1.21.4 */ && PROTOCOL_VERSION < 775 /* < 26.1 */
 #include "protocolCraft/Types/Either.hpp"
 #endif
 #include "protocolCraft/Types/Components/Subtypes/Instrument.hpp"
@@ -15,10 +15,10 @@ namespace ProtocolCraft
         {
 #if PROTOCOL_VERSION < 770 /* < 1.21.5 */
             SERIALIZED_FIELD(Instrument, Holder<ProtocolCraft::Components::Instrument>);
-#elif PROTOCOL_VERSION < 775 /* > 26.1 */
+#elif PROTOCOL_VERSION < 775 /* < 26.1 */
             SERIALIZED_FIELD(Instrument, Either<Holder<ProtocolCraft::Components::Instrument>, Identifier>);
 #else
-            SERIALIZED_FIELD(Instrument, ProtocolCraft::Components::Instrument);
+            SERIALIZED_FIELD(Instrument, Holder<ProtocolCraft::Components::Instrument>);
 #endif
 
             DECLARE_READ_WRITE_SERIALIZE;
--- protocolCraft/include/protocolCraft/Types/Components/Subtypes/Instrument.hpp
+++ protocolCraft/include/protocolCraft/Types/Components/Subtypes/Instrument.hpp
@@ -12,7 +12,11 @@ namespace ProtocolCraft
         class Instrument : public NetworkType
         {
             SERIALIZED_FIELD(SoundEvent, Holder<ProtocolCraft::SoundEvent>);
+#if PROTOCOL_VERSION < 768 /* < 1.21.2 */
+            SERIALIZED_FIELD(UseDuration, VarInt);
+#else
             SERIALIZED_FIELD(UseDuration, float);
+#endif
             SERIALIZED_FIELD(Range, float);
             SERIALIZED_FIELD(Description, Chat);
 
