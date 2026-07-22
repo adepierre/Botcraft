8d96f75c0512672a44c57b23f37f558f9ff2d9d1
adepierre <24371370+adepierre@users.noreply.github.com>
2026-07-23 00:52:50 +0100

    Fix JukeboxPlayable component for 26.2+

--- protocolCraft/include/protocolCraft/Types/Components/DataComponentTypeJukeboxPlayable.hpp
+++ protocolCraft/include/protocolCraft/Types/Components/DataComponentTypeJukeboxPlayable.hpp
@@ -2,9 +2,9 @@
 #pragma once
 #include "protocolCraft/Types/Components/DataComponentType.hpp"
 #include "protocolCraft/Types/Components/Subtypes/JukeboxSong.hpp"
+#include "protocolCraft/Types/Holder.hpp"
 #if PROTOCOL_VERSION < 775 /* < 26.1 */
 #include "protocolCraft/Types/Either.hpp"
-#include "protocolCraft/Types/Holder.hpp"
 #include "protocolCraft/Types/Identifier.hpp"
 #endif
 
@@ -17,7 +17,7 @@ namespace ProtocolCraft
 #if PROTOCOL_VERSION < 775 /* < 26.1 */
             SERIALIZED_FIELD(Song, Either<Holder<JukeboxSong>, Identifier>);
 #else
-            SERIALIZED_FIELD(Song, JukeboxSong);
+            SERIALIZED_FIELD(Song, Holder<JukeboxSong>);
 #endif
 #if PROTOCOL_VERSION < 770 /* < 1.21.5 */
             SERIALIZED_FIELD(ShowInTooltip, bool);
