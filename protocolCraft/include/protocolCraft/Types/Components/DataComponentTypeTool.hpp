6b7c781d52b1a674c027cf869b6b937fa53e5797
adepierre <24371370+adepierre@users.noreply.github.com>
2026-07-23 02:07:10 +0100

    Fix DataComponentTypeTool for 1.21.5+

--- protocolCraft/include/protocolCraft/Types/Components/DataComponentTypeTool.hpp
+++ protocolCraft/include/protocolCraft/Types/Components/DataComponentTypeTool.hpp
@@ -15,7 +15,7 @@ namespace ProtocolCraft
             SERIALIZED_FIELD(DefaultMiningSpeed, float);
             SERIALIZED_FIELD(DamagePerBlock, VarInt);
 #if PROTOCOL_VERSION > 769 /* > 1.21.4 */
-            SERIALIZED_FIELD(CanDestroyBlocksInCreative, VarInt);
+            SERIALIZED_FIELD(CanDestroyBlocksInCreative, bool);
 #endif
 
             DECLARE_READ_WRITE_SERIALIZE;
