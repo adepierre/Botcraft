68a303ca6eef30d5a34655e3d7a8c8b092bedd28
adepierre <24371370+adepierre@users.noreply.github.com>
2026-07-23 01:41:48 +0100

    Fix beehive component data for 1.21.9+

--- protocolCraft/include/protocolCraft/Types/Components/Subtypes/BeehiveBlockEntityOccupant.hpp
+++ protocolCraft/include/protocolCraft/Types/Components/Subtypes/BeehiveBlockEntityOccupant.hpp
@@ -1,7 +1,12 @@
 #if PROTOCOL_VERSION > 765 /* > 1.20.4 */
 #pragma once
 #include "protocolCraft/NetworkType.hpp"
+#if PROTOCOL_VERSION < 773 /* < 1.21.9 */
 #include "protocolCraft/Types/Components/DataComponentTypeCustomData.hpp"
+#else
+#include "protocolCraft/Types/Components/DataComponentTypeTypedEntityData.hpp"
+#endif
+
 
 namespace ProtocolCraft
 {
@@ -9,7 +14,11 @@ namespace ProtocolCraft
     {
         class BeehiveBlockEntityOccupant : public NetworkType
         {
+#if PROTOCOL_VERSION < 773 /* < 1.21.9 */
             SERIALIZED_FIELD(EntityData, DataComponentTypeCustomData);
+#else
+            SERIALIZED_FIELD(EntityData, DataComponentTypeTypedEntityData);
+#endif
             SERIALIZED_FIELD(TicksInHive, VarInt);
             SERIALIZED_FIELD(MinTicksInHive, VarInt);
 
